172329f1c2b7f8c175f7b85ea6f9819cce1fffe2
HanatYoma <47262418+Whatdapep@users.noreply.github.com>
2020-07-15 23:44:00 +0700

    pom first commit

---
+++ src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <label for="word">insert Words Matching</label>
+    <input type="text" name="words" v-model="words" id="words" />
+    <button type="button" @click="HandlerWordsClick">
+      Ok
+    </button>
+
+    <div id="wordsOutput">
+      {{ outputWords }}
+    </div>
+
+    <TableResult v-bind:props="outputWords" />
+  </div>
+</template>
+<script>
+import TableResult from './TableResult';
+export default {
+  name: 'Main',
+  components:{
+TableResult
+  },
+  data: function () {
+    return {
+      words: '',
+      outputWords: ''
+    }
+  },
+  methods: {
+    HandlerWordsClick: function () {
+      var propArr = this.words.split(' ')
+    //   var c_count = propArr.length
+
+      this.outputWords = propArr
+    }
+  }
+}
+</script>
+<style scoped></style>
---
+++ src/components/TableResult.vue
@@ -0,0 +1,27 @@
+<template>
+    <table>
+    <tbody>
+    <v-fo></v-fo>
+    <tr v-for="result in props" v-bind:key="result" >
+    {{result}}
+    </tr>
+    </tbody>
+    </table>
+</template>
+<script>
+export default {
+    name:'TableResult',
+    data:function ()
+    {
+        return {
+            props:null,
+
+        }
+    }
+    ,
+    props:['props']
+}
+</script>
+<style scoped>
+
+</style>
