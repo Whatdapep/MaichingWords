91d8a23a2ca0262bb747f06a2737184ac7bc7dd6
HanatYoma <47262418+Whatdapep@users.noreply.github.com>
2020-07-20 23:21:44 +0700

    pom

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueTailwind from 'vue-tailwind'
 
-
+import './assets/styles/index.css';
 
 Vue.config.productionTip = false
 Vue.use(VueTailwind)
